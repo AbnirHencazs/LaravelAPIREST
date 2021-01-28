ea1e7cdcb026fa3c3da6468fe99a1b4872233d0c
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-01-28 09:09:32 -0600

    tailwindcss styles on index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,15 +5,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        
+
         <title>Laravel</title>
     </head>
-    <body>
-        @foreach( $posts as $post )
-            <div>
-                <h2>{{ $post->title }}</h2>
+    <body class="bg-gray-100 text-gray-700">
+        <div class="container mx-auto px-4">
+            <div class="grid grid-cols-3 my-10">
+                @foreach( $posts as $post )
+                    <div class="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                        <h2 class="font-bold text-lg mb-4">{{ $post->title }}</h2>
+                        <p class="text-sx">{{ $post->excerpt }}</p>
+                        <p class="text-xs text-right">{{ $post->published_at }}</p>
+                    </div>
+                @endforeach    
             </div>
-        @endforeach
-        {{ $posts->links() }}
+            {{ $posts->links() }}
+        </div>
     </body>
 </html>
