dc68c564f7483ef8afebd943fbf3f958272558cd
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-01-28 09:36:40 -0600

    Defined the format for the resource Post

--- app/Http/Resources/V1/PostResource.php
+++ app/Http/Resources/V1/PostResource.php
@@ -14,6 +14,12 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'excerpt' => $this->excerpt,
+            'content' => $this->content
+        ];
     }
 }
